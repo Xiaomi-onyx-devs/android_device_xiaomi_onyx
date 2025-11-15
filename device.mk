603422c367d70706b0b57f2e79d856e6adafa608
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2025-11-15 17:06:06 +0100

    onyx: rootdir: Set default thermal profile
    - Sets the default thermal performance profile to optimize device temperature management
      and power consumption under typical usage scenarios.
    
    Change-Id: I13c3c909f75ba5dd129e0990c1e4e033201aada0

--- device.mk
+++ device.mk
@@ -143,7 +143,7 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.composer3-V3-ndk.vendor \
     vendor.qti.hardware.display.aiqe-V2-ndk.vendor  \
     vendor.qti.hardware.display.config-V12-ndk.vendor  \
-    vendor.qti.hardware.display.composer3-V1-ndk.vendor 
+    vendor.qti.hardware.display.composer3-V1-ndk.vendor
 
 # DRM
 PRODUCT_PACKAGES += \
@@ -209,6 +209,7 @@ PRODUCT_PACKAGES += \
     ueventd.qcom.rc
 
 PRODUCT_PACKAGES += \
+    init.mi_perf.rc \
     init.qcom.factory.rc \
     init.qcom.rc \
     init.recovery.qcom.rc \
